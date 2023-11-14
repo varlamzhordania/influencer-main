ab9f6d1e4e609c65c174381949c6abb69a57a550
AmirBigdeli <141164942+varlamzhordania@users.noreply.github.com>
2023-11-14 19:44:49 +0400

    test images

--- src/components/home/Choose.jsx
+++ src/components/home/Choose.jsx
@@ -1,27 +1,32 @@
 import React from 'react'
 import Wrapper from '../shared/Wrapper'
 import Image from 'next/image'
-import { Raleway } from 'next/font/google'
-import { RevealWrapper } from 'next-reveal'
+import {Raleway} from 'next/font/google'
+import {RevealWrapper} from 'next-reveal'
+import chooseIcon1 from "./../../../public/images/choose-icon1.png"
+import chooseIcon2 from "./../../../public/images/choose-icon2.png"
+import chooseIcon3 from "./../../../public/images/choose-icon3.png"
+import chooseIcon4 from "./../../../public/images/choose-icon4.png"
+import chooseUs from "./../../../public/images/choose-us.png"
 
-const font = Raleway({ subsets: ['latin'] })
+const font = Raleway({subsets: ['latin']})
 const Choose = () => {
     const data = [
         {
             id: 1,
-            img: '/images/choose-icon1.png',
+            img: chooseIcon1,
             title: 'Extensive Influencer Network:',
             text: 'Access 300,000+ Canadian influencers for local collaborations and increased brand visibility.'
         },
         {
             id: 2,
-            img: '/images/choose-icon1.png',
+            img: chooseIcon1,
             title: 'Budget-Friendly:',
             text: 'Affordable and effective influencer marketing solutions for businesses of all sizes.'
         },
         {
             id: 3,
-            img: '/images/choose-icon3.png',
+            img: chooseIcon3,
             title: 'Inclusive Campaigns:',
             text: 'Reach diverse Canadian audiences effectively with impactful campaigns.'
         },
@@ -29,19 +34,19 @@ const Choose = () => {
     const data1 = [
         {
             id: 4,
-            img: '/images/choose-icon1.png',
+            img: chooseIcon1,
             title: 'Canadian Market Expertise:',
             text: 'Tailored solutions for international brands and local businesses to succeed in the Canadian market.'
         },
         {
             id: 5,
-            img: '/images/choose-icon2.png',
+            img: chooseIcon2,
             title: 'Local Support:',
             text: 'Dedicated local team without agency fees, ensuring successful influencer campaigns.'
         },
         {
             id: 6,
-            img: '/images/choose-icon2.png',
+            img: chooseIcon2,
             title: 'Influencer-Generated Content:',
             text: 'Cost-effective content creation by nano and micro-influencers, optimizing your marketing budget.'
         },
@@ -51,13 +56,18 @@ const Choose = () => {
             <Wrapper>
                 <div className='flex md:flex-row flex-col justify-start items-start gap-16'>
                     <div className='md:w-1/2'>
-                        <RevealWrapper rotate={{ x: 0, y: 40, z: 0 }} origin='bottom' delay={200} duration={3000} distance='500px' reset={false} viewOffset={{ top: 0, right: 0, bottom: 10, left: 0 }}>
+                        <RevealWrapper rotate={{x: 0, y: 40, z: 0}} origin='bottom' delay={200} duration={3000}
+                                       distance='500px' reset={false}
+                                       viewOffset={{top: 0, right: 0, bottom: 10, left: 0}}>
 
-                            <Image src={'/images/choose-us.png'} width={1080} height={1080} alt="" className='w-[501px] ' />
+                            <Image src={chooseUs} width={1080} height={1080} alt=""
+                                   className='w-[501px] '/>
                         </RevealWrapper>
                     </div>
                     <div className='md:w-1/2'>
-                        <RevealWrapper rotate={{ x: 0, y: 40, z: 0 }} origin='bottom' delay={200} duration={3000} distance='500px' reset={false} viewOffset={{ top: 0, right: 0, bottom: 10, left: 0 }}>
+                        <RevealWrapper rotate={{x: 0, y: 40, z: 0}} origin='bottom' delay={200} duration={3000}
+                                       distance='500px' reset={false}
+                                       viewOffset={{top: 0, right: 0, bottom: 10, left: 0}}>
                             <div className='flex flex-col justify-start items-start'>
                                 <p className={`${font.className} text-[32px] md:text-[34px] font-[700]`}>
                                     Why Choose Us:
@@ -73,8 +83,10 @@ const Choose = () => {
                                         {
                                             data.map((item) => (
                                                 <div key={item.id} className='flex items-start justify-start gap-3'>
-                                                    <div className='bg-white w-[50px] h-[50px] flex items-center justify-center rounded-md border-r border-b border-[#AF1FFC]'>
-                                                        <Image src={item.img} alt='' width={500} height={500} className='w-[26px] h-[26px]' />
+                                                    <div
+                                                        className='bg-white w-[50px] h-[50px] flex items-center justify-center rounded-md border-r border-b border-[#AF1FFC]'>
+                                                        <Image src={item.img} alt='' width={500} height={500}
+                                                               className='w-[26px] h-[26px]'/>
                                                     </div>
                                                     <div className='flex flex-col justify-start items-start'>
                                                         <p className={`${font.className} text-[16px] font-[600]`}>
@@ -92,8 +104,10 @@ const Choose = () => {
                                         {
                                             data1.map((item) => (
                                                 <div key={item.id} className='flex items-start justify-start gap-3'>
-                                                    <div className='bg-white w-[50px] h-[50px] flex items-center justify-center rounded-md border-r border-b border-[#AF1FFC]'>
-                                                        <Image src={item.img} alt='' width={500} height={500} className='w-[26px] h-[26px]' />
+                                                    <div
+                                                        className='bg-white w-[50px] h-[50px] flex items-center justify-center rounded-md border-r border-b border-[#AF1FFC]'>
+                                                        <Image src={item.img} alt='' width={500} height={500}
+                                                               className='w-[26px] h-[26px]'/>
                                                     </div>
                                                     <div className='flex flex-col justify-start items-start'>
                                                         <p className={`${font.className} text-[16px] font-[600]`}>
